99d451dd82e82e89793d9336ee51dbf06d2c510f
chenlixiyuCBB <chenlixiyuCBB@github.com>
2016-11-17 16:17:29 +0800

    完成爬虫（未测试）

--- sina_search/pipelines.py
+++ sina_search/pipelines.py
@@ -1,45 +1,15 @@
 # encoding=utf-8
 import pymongo
-from items import weiboItem
 
 
 class MongoDBPipleline(object):
     def __init__(self):
         clinet = pymongo.MongoClient("localhost", 27017)
         db = clinet["Sina"]
-        self.Information = db["Information"]
-        self.Tweets = db["Tweets"]
-        self.Follows = db["Follows"]
-        self.Fans = db["Fans"]
+        self.weibo = db["weibo"]
 
     def process_item(self, item, spider):
-        """ 判断item的类型，并作相应的处理，再入数据库 """
-        if isinstance(item, InformationItem):
-            try:
-                self.Information.insert(dict(item))
-            except Exception:
-                pass
-        elif isinstance(item, TweetsItem):
-            try:
-                self.Tweets.insert(dict(item))
-            except Exception:
-                pass
-        elif isinstance(item, FollowsItem):
-            followsItems = dict(item)
-            follows = followsItems.pop("follows")
-            for i in range(len(follows)):
-                followsItems[str(i + 1)] = follows[i]
-            try:
-                self.Follows.insert(followsItems)
-            except Exception:
-                pass
-        elif isinstance(item, FansItem):
-            fansItems = dict(item)
-            fans = fansItems.pop("fans")
-            for i in range(len(fans)):
-                fansItems[str(i + 1)] = fans[i]
-            try:
-                self.Fans.insert(fansItems)
-            except Exception:
-                pass
-        return item
+        try:
+            self.weibo.insert(dict(item))
+        except:
+            pass
--- sina_search/spiders/spiders.py
+++ sina_search/spiders/spiders.py
@@ -2,23 +2,24 @@
 from scrapy.spider import CrawlSpider
 from scrapy.selector import Selector
 from sina_search.items import weiboItem
+from scrapy.http import Request
 
 
 class Spider(CrawlSpider):
     name = "sina_search"
-    host = "http://weibo.cn"
+    allowed_domains = ["http://weibo.cn"]
     page = 1
-    urls = "http://weibo.cn/search/mblog?hideSearchFrame=&keyword=情话&page=%s" %page
+    urls = "http://weibo.cn/search/mblog?hideSearchFrame=&keyword=情话&page=%s" % page
     start_urls = [urls]
-    scrawl_ID = set(start_urls)  # 记录待爬的微博ID
-    finish_ID = set()  # 记录已爬的微博ID
-
 
     def parse(self, response):
-      weibos = Selector(response).xpath('//div[@class="c"]')
-
-      for  weibo in weibos:
-          item = weiboItem()
-          item['ID'] = weibo.xpath('@id').extract()
-          item['Text'] = weibo.xpath('span[@class="ctt]/text()').extract()
-          yield item
+        weibos = Selector(response).xpath('//div[@class="c"]')
+        if weibos is not None:
+            for weibo in weibos:
+                item = weiboItem()
+                item['ID'] = weibo.xpath('@id').extract()
+                item['Text'] = weibo.xpath('span[@class="ctt]/text()').extract()
+                yield item
+            self.page += 1
+            yield Request(url=self.urls, meta={"item": weiboItem}, callback=self.parse)
+        print "爬取完毕"
